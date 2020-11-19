4568a20cc3c40ad08182c12a0261afe259fc52b5
Imadi64 <Imadi64@bk.ru>
2020-11-20 01:29:23 +0400

    test

--- игра tetris.py
+++ игра tetris.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
+
 import sys, random
 from PyQt5.QtWidgets import QMainWindow, QFrame, QDesktopWidget, QApplication
 from PyQt5.QtCore import Qt, QBasicTimer, pyqtSignal
